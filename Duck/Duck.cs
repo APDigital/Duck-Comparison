5d5da265f7b7465324e27c2636b2adde8154cfea
APDigital <aoifejo.pearson@googlemail.com>
2018-01-25 09:57:29 +0000

    override Equals and GetHashCode in order to use the Duck class as a Key in a dictionary

--- Duck/Duck.cs
+++ Duck/Duck.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Duck
+namespace Comparison
 {
     public class Duck
     {
@@ -20,6 +20,29 @@ namespace Duck
             WeightInGrams = weightInGrams;
             AgeInMonths = ageInMonths;
         }
+        public override bool Equals(object obj)
+        {
+            Duck d2 = obj as Duck;
+            if (d2 == null)
+            {
+                return false;
+            }
+            return Name == d2.Name && Type == d2.Type && WeightInGrams == d2.WeightInGrams && AgeInMonths == d2.AgeInMonths;  
+        }
+        //followed explanation from https://stackoverflow.com/questions/263400/what-is-the-best-algorithm-for-an-overridden-system-object-gethashcode
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 29 + (Name ?? "").GetHashCode();
+                hash = hash * 29 + (Type ?? "").GetHashCode();
+                hash = hash * 29 + (WeightInGrams).GetHashCode();
+                hash = hash * 29 + (AgeInMonths).GetHashCode();
+                return hash;
+            }
+        }
+
     }
 
    
--- Duck/Program.cs
+++ Duck/Program.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Duck
+namespace Comparison
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var quack = new Duck("Sir Quackalot", "Mallard", 100, 17);
+            Dictionary<Duck, string> dictionary = new Dictionary<Duck, string>();
+            dictionary.Add(new Duck("Sir Quackalot", "Mallard", 100, 17),"");
         }
     }
 }
